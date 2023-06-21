344c3a086e707b018c861069bc4d5a138df3cd53
joelleeyueer <35606477+joelleeyueer@users.noreply.github.com>
2023-06-21 19:49:53 +0800

    Angular to get current location

--- client/src/app/cat-map/cat-map.component.ts
+++ client/src/app/cat-map/cat-map.component.ts
@@ -16,27 +16,82 @@ export class CatMapComponent implements OnInit {
   cats: Cat[] = [];
 
 
+  // ngOnInit(): void {
+  //   const loader = new Loader({
+  //     apiKey: 'AIzaSyDXy7SLzzZRQJyDba-z8DGqWXD-8xHQVhQ',
+  //     version: "weekly",
+  //   });
+
+  //   loader.load().then(() => {
+  //     console.log("Google Maps loaded");
+  //     const location = {
+  //       lat: 1.3537,
+  //       lng: 103.7190
+  //     }
+
+  //     this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  //       center: location,
+  //       zoom: 16
+  //     });
+
+  //     this.updateMarkers()
+  //   });
+  // }
+
   ngOnInit(): void {
     const loader = new Loader({
       apiKey: 'AIzaSyDXy7SLzzZRQJyDba-z8DGqWXD-8xHQVhQ',
       version: "weekly",
     });
-
+  
     loader.load().then(() => {
       console.log("Google Maps loaded");
-      const location = {
-        lat: 1.3537,
-        lng: 103.7190
-      }
-
       this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: location,
         zoom: 16
       });
-
-      this.updateMarkers()
+  
+      // Try HTML5 geolocation
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+  
+            // Set the map's center to the user's location
+            if (this.map) this.map.setCenter(pos);
+            this.updateMarkers()
+          },
+          () => {
+            // If geolocation fails, set the center to a default location
+            this.handleLocationError(true, {lat: 1.3537, lng: 103.7190});
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation, set the center to a default location
+        this.handleLocationError(false, {lat: 1.3537, lng: 103.7190});
+      }
     });
   }
+
+  handleLocationError(browserHasGeolocation: boolean, pos: google.maps.LatLngLiteral) {
+    if (!this.map) return;
+  
+    this.map.setCenter(pos);
+  
+    // You can also show an info window with the error message
+    const infoWindow = new google.maps.InfoWindow({
+      content: browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation.",
+      position: pos,
+    });
+  
+    infoWindow.open(this.map);
+}
+
+  
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['cats'] && !changes['cats'].isFirstChange() && this.map) {
